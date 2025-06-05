150d1e4185c4d204ee345ff16fbd543284a6f6b1
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-06-05 16:04:11 +0200

    mise à jour de la page évasion

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,9 @@
 
-    // Affiche la pop-up automatiquement au chargement de la page
+    // Affiche la pop-up automatiquement après un délais automatique après 2 secondes (2000ms)
 window.onload = function() {
-  document.getElementById('popup').classList.add('active');
+  setTimeout(function() {
+  document.getElementById('popup').classList.add('active');  
+  }, 2000);
 };
 
 // Ferme la pop-up au clic sur la croix
@@ -9,3 +11,5 @@ document.getElementById('closeBtn').onclick = function() {
   document.getElementById('popup').classList.remove('active');
 };
  
+
+  
