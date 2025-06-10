2d4eccf78d26c5e594298132ed1fa1525f32843c
M-BEDH <mel.b.ms@pm.me>
2025-06-10 23:37:32 +0200

    correction commentaire

--- javascript/scriptCookie.js
+++ javascript/scriptCookie.js
@@ -1,21 +1,21 @@
-   // Affiche la pop-up automatiquement après un délais automatique après 2 secondes (2000ms)
+   // Affichele bandeau cookie automatiquement après un délais automatique après 2 secondes (2000ms)
 window.onload = function() {
   setTimeout(function() {
   document.getElementById('cookies').classList.add('active');  
   }, 0);
 };
 
-// Ferme la pop-up au clic sur la croixAdd commentMore actions
+// Fermele bandeau cookie au clic sur la croixAdd commentMore actions
 document.getElementById('closeBtn').onclick = function() {
   document.getElementById('cookies').classList.remove('active');
 };
 
-// Ferme la pop-up au clic sur le bouton "Accepter"
+// Fermele bandeau cookie au clic sur le bouton "Accepter"
 document.getElementById('acceptBtn').onclick = function() {
   document.getElementById('cookies').classList.remove('active');
 };
 
-// Ferme la pop-up au clic sur le bouton "Refuser"
+// Fermele bandeau cookie au clic sur le bouton "Refuser"
 document.getElementById('rejectBtn').onclick = function() {
   document.getElementById('cookies').classList.remove('active');
 };
