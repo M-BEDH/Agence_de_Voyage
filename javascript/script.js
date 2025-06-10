53fcb62547b0d0423d40e622686e33223b3e743f
M-BEDH <mel.b.ms@pm.me>
2025-06-10 20:42:25 +0200

    creation cookies

--- javascript/script.js
+++ javascript/script.js
@@ -11,5 +11,25 @@ document.getElementById('closeBtn').onclick = function() {
   document.getElementById('popup').classList.remove('active');
 };
  
+    // Affiche la pop-up automatiquement après un délais automatique après 2 secondes (2000ms)
+window.onload = function() {
+  setTimeout(function() {
+  document.getElementById('cookies').classList.add('active');  
+  }, 0);
+};
 
+// Ferme la pop-up au clic sur la croix
+document.getElementById('closeBtn').onclick = function() {
+  document.getElementById('cookies').classList.remove('active');
+};
+
+// Ferme la pop-up au clic sur le bouton "Accepter"
+document.getElementById('acceptBtn').onclick = function() {
+  document.getElementById('cookies').classList.remove('active');
+};
+
+// Ferme la pop-up au clic sur le bouton "Refuser"
+document.getElementById('rejectBtn').onclick = function() {
+  document.getElementById('cookies').classList.remove('active');
+};
   
